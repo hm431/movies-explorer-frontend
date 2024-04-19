288abca60a6d41da1edd97b8815c1c1cd2f10056
Чучалин Андрей <aic7120@gmail.com>
2024-04-19 07:34:16 +0300

    fix routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import Login from './Login';
 import Registration from './Registration';
 import Main from './Main';
 import Saved from './Saved';
-
+import NotFound from "./NotFound";
 
 
 
@@ -15,7 +15,7 @@ import Saved from './Saved';
 function App() {
 
 
-  
+
   const [loggedIn, setLoggedIn] = useState(localStorage.getItem('jwt'));
 
   const handleLogin = () => {
@@ -23,41 +23,47 @@ function App() {
   }
 
   return (
-    <div className="App">  
+    <div className="App">
 
 
-<BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route
             path="/"
-            element={<Lending/>}/>
+            element={<Lending />} />
 
           <Route
-            path="/my-profile"
+            path="/profile"
             element={<ProtectedRoute
               loggedIn={loggedIn}
               element={Profile} />} />
 
           <Route
-            path="/singUp"
-            element={<Registration/>} />
+            path="/singup"
+            element={<Registration />} />
 
           <Route
-            path="/login"
+            path="/signin"
             element={<Login />} />
 
           <Route
-          path='/main'
-          element={<Main/>}/>
-              
-            <Route
+            path='/main'
+            element={<movies />} />
+
+
+          <Route
             path='/saved'
-            element={<Saved />} />
+            element={<saved-movies/>} />
+
+
+          <Route
+            path="/404"
+            element={<NotFound />} />
         </Routes>
 
       </BrowserRouter>
 
-      
+
     </div>
   );
 }
